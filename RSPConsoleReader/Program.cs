4ef3c5eeffc779351be0f78fc067ae49572e2841
Greg <dev.pollux@gmail.com>
2015-11-19 23:09:54 +0100

    initial commit Lecture RSP simple. structure lineaire (non nested)

---
+++ RSPConsoleReader/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using rspDll;
+
+
+namespace RSPConsoleReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rspPath = @"H:\FichiersB2\retours\69080041\";
+            
+            //Deux methodes pour enumerer les fichiers :
+            //Methode 1 : GetFile Renvois seulement les noms des fichiers (en string)
+            IEnumerable<string> fileList = System.IO.Directory.GetFiles(rspPath,"*.rsp");
+            foreach(string file in fileList){
+                Console.WriteLine(file);
+            }
+            Console.WriteLine("Nous avons trouvé {0} fichiers rsp", fileList.Count());
+
+            //Methode 2 : DirectoryInfo.EnumerateFiles renvois des FileInfo (plus proche de ce que je connais en pshell)
+            System.IO.DirectoryInfo dossier = new System.IO.DirectoryInfo(rspPath);
+            IEnumerable<FileInfo> fichiers = dossier.EnumerateFiles("*.rs_",System.IO.SearchOption.TopDirectoryOnly);
+            List<RspFile> retours = new List<RspFile>();
+            
+            //On creer un RspFile pour chaque fichier (Le parsing a lieu ici)
+            foreach (FileInfo file in fichiers)
+            {
+                //Console.WriteLine("{0}:{1}",file.Name,(float)file.Length/1000);
+                try
+                {
+                    retours.Add(new RspFile(file.FullName));
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} CANNOT BE PARSED", file.FullName);
+                    Console.ResetColor();
+                }
+            }
+            
+            //on affiche pour tout les retours la ref trouvée. (pour verifier que le parsing est ok)
+            foreach (RspFile retour in retours){
+                //Console.WriteLine(retour.reference);
+            }
+
+            //Pour le premier 
+            RspFile retour0 = retours.First();//First est une methode etendues, definie par linq.
+            /*
+            foreach (string line in retour0.rawEntite){
+                int pad = 0;
+                Int32.TryParse(line.Substring(3, 2), out pad);
+                if (pad == 99) { pad = 12; } else { pad *= 2; }
+                Console.WriteLine("{0}",line.Substring(0,3).PadLeft(pad));
+            }
+            */
+            /*Console.WriteLine("retour0 : {0} entites, {1} lines ", retour0.entites.Count , retour0.rawEntite.Count() );
+            foreach (Entite e in retour0.entites)
+            {
+                Console.WriteLine("{0}: {1}", e.type, e.data.PadLeft(e.data.Length+e.level%10));
+            }*/
+
+            Console.WriteLine(rspDll.Entite.wololo());
+        }
+    }
+}
---
+++ rspDll/rspDll.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace rspDll
+{
+    public class Entite
+    {
+        public string type { get; set; }
+        public int level { get; set; }
+        public string data { get; set; }
+        public ObservableCollection<Entite> subs { get; set; }
+        //public readableData dictionnary //contient les données parsées
+
+        public Entite(string data)
+        {
+            this.data = data;
+            this.subs = new ObservableCollection<Entite>();
+            this.type = data.Substring(0, 3);
+            
+            if (this.type == "000" || this.type == "999") {
+                this.level = 0;
+            } else {
+                this.level = int.Parse(this.data.Substring(3, 2));
+            }
+        }
+
+        
+        //dummy static function
+        public static string wololo()
+        {
+            return "wololo";
+        }
+    }
+
+
+    public class RspFile
+    {
+        public string filePath { get; set; }
+        public string[] rawEntite { get; set; }
+        public string reference { get; set; }
+        public ObservableCollection<Entite> entites { get; set; }
+
+        public RspFile()
+        {
+            this.entites = new ObservableCollection<Entite>();
+        }
+
+        public RspFile(string path)
+        {
+            this.filePath = path;
+            this.rawEntite = (File.ReadAllText(path)).Split('@');
+            this.reference = rawEntite[0].Substring(61, 3);
+            this.entites = new ObservableCollection<Entite>();
+            this.parse();
+        }
+
+        public void parse() { //Version 1 : l'objet RspFile contient la liste de toutes les entités.
+            foreach (string line in this.rawEntite)
+            {
+                this.entites.Add(new Entite(line));
+            }
+        }
+    }
+
+    public class RspFilter
+    {
+        public bool active { get; set; }
+        public int entiteType { get; set; }
+        public int color;//pour differencier les filtres sur une même liste.
+
+        public RspFilter()
+        {
+            this.active = true;
+        }
+    }
+
+    public class RspList
+    {
+        private ObservableCollection<RspFile> membres;
+        //private ObservableCollection<RspFilter> filters;
+    }
+
+
+}
